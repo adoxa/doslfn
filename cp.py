6ae3002f5db912e1f0d7cb1d86b6b9a57c9a86ce
Jason Hood <jadoxa@yahoo.com.au>
2025-08-08 18:21:24 +1000

    Add more languages
    Wilhelm Spiegl used Gemini to automatically generate some languages.
    Not perfect (on/off & email/download were mostly missed, and I think
    "read-only bit" didn't translate well), but maybe a starting point.

--- cp.py
+++ cp.py
@@ -4,16 +4,30 @@
 # Python 2, as I don't think there's a 3 for DOS, in case someone wants to run
 # this natively (not that I tested it).
 
+import sys
+
 
 cp = {
-  "de": "cp437",
-  "en": "cp437",
-  "es": "cp437",
-  "fr": "cp850",
-  "ja": "cp932",
-  "ru": "cp866",
-  "tr": "cp857",
-  "zh": "cp936"
+  "cz": "CP852",
+  "de": "CP437",
+  "dk": "CP865",
+  "en": "CP437",
+  "es": "CP850",
+  "fi": "CP850",
+  "fr": "CP850",
+  "hu": "CP852",
+  "it": "CP850",
+  "ja": "CP932",
+  "lv": "CP775",
+  "nl": "CP850",
+  "no": "CP865",
+  "pl": "CP852",
+  "pt": "CP860",
+  "ru": "CP866",
+  "sl": "CP852",
+  "sv": "CP850",
+  "tr": "CP857",
+  "zh": "CP936"
 }
 
 
@@ -22,11 +36,15 @@ def tocp(name, page):
     f = open(name, "rb")
   except:
     # print "Ignoring ", name
-    return
+    return None
   utf8 = f.read()
   f.close()
   uc = utf8.decode("utf8")
-  dos = uc.encode(page)
+  try:
+    dos = uc.encode(page)
+  except UnicodeEncodeError as e:
+    print "Failed to encode", repr(e.object[e.start-5 : e.end+5]), "using", page + "."
+    return False
   cpname = name[:4] + "DOSLFN." + name[4:6].upper()
   if name.startswith("doc"):
     cpname = cpname.lower()
@@ -34,7 +52,17 @@ def tocp(name, page):
   f.write(dos)
   f.close()
   print "Converted", name, "to", cpname
+  return True
 
-for lang in cp:
+
+if len(sys.argv) == 1:
+  for lang in cp:
     tocp("doc/" + lang + ".txt", cp[lang])
     tocp("NLS/" + lang + ".txt", cp[lang])
+else:
+  for arg in sys.argv[1:]:
+    lang = arg[4:6]
+    if lang in cp:
+      tocp(arg, cp[lang])
+    else:
+      print arg, "has unknown lang:", lang
